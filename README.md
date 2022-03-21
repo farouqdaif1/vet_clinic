a00c29da988b9c75d2bffdefeba3716f8d783d34
farouqdaif1 <daiffarouq@gmail.com>
2022-03-21 17:15:31 +0200

    Create database

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# vet_clinic
+# curriculum-databases-projects-template
+
+> In this project I've created a database use a relational database to create the initial data structure for a vet clinic. You will create a table to store animals' information, insert some data into it, and query it.
+
+
+## Getting Started
+
+This repository includes files with plain SQL that can be used to recreate a database:
+
+- Use [schema.sql](./schema.sql) to create all tables.
+- Use [data.sql](./data.sql) to populate tables with sample data.
+- Check [queries.sql](./queries.sql) for examples of queries that can be run on a newly created database. **Important note: this file might include queries that make changes in the database (e.g., remove records). Use them responsibly!**
+
+
+## Authors
+
+👤 **Farouq Daif**
+
+- GitHub: [@farouqdaif1](https://github.com/farouqdaif1)
+- LinkedIn: [@Farouq Daif ](https://www.linkedin.com/in/farouqdaif/)
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ queries.sql
@@ -0,0 +1,44 @@
+--Find all animals whose name ends in "mon".
+
+select * from animals where name like '%mon';
+--   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23
+--   2 | Gabumon | 2018-11-15    |               2 | t        |       8.0
+--   4 | Devimon | 2017-05-12    |               5 | t        |      11.0
+
+--List the name of all animals born between 2016 and 2019.
+
+select name from animals where date_of_birth between '2016-01-01'and '2019-12-31';
+--  Gabumon
+--  Devimon
+
+--List the name of all animals that are neutered and have less than 3 escape attempts.
+select name from animals where neutered= true and escape_attempts<=3;
+--  Agumon
+--  Gabumon
+
+--List date of birth of all animals named either "Agumon" or "Pikachu".
+select date_of_birth from  animals where name='Agumon' or name='Pikachu' ;
+--  2020-02-03
+--  2021-01-07
+
+--List name and escape attempts of animals that weigh more than 10.5kg
+select name ,escape_attempts from animals where weight_kg > 10.5;
+--  Pikachu |               1
+--  Devimon |               5
+
+--Find all animals that are neutered.
+select name from animals where neutered =true;
+--  Agumon
+--  Gabumon
+--  Devimon
+
+--Find all animals not named Gabumon.
+select * from animals where not name='Gabumon';
+--   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     15.04
+--   4 | Devimon | 2017-05-12    |               5 | t        |      11.0
+
+--Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
+ select  name from  animals where  weight_kg between 10.4 and 17.3 ;
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     15.04
+--   4 | Devimon | 2017-05-12    |               5 | t        |      11.0
