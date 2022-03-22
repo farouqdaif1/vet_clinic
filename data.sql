fe16401b57422dc996530187d98618b2420ec2f1
farouqdaif1 <daiffarouq@gmail.com>
2022-03-22 15:49:47 +0200

    Add update and delet qureies

--- data.sql
+++ data.sql
@@ -11,3 +11,42 @@ VALUES ('Pikachu','2021-1-7',1,false,15.04);
 insert into animals(name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES ('Devimon','2017-5-12',5,true,11.0);
 --INSERT 0 1
+------------------------------------------------------------------Day 2-------------------------------
+
+--Animal: His name is Charmander. He was born on Feb 8th, 2020, and currently weighs -11kg. He is not neutered and he has never tried to escape.
+
+insert into animals(name ,date_of_birth ,weight_kg ,neutered ,escape_attempts)
+values('Charmander','2020-02-08',-11,false,0);
+
+--Animal: Her name is Plantmon. She was born on Nov 15th, 2022, and currently weighs -5.7kg. She is neutered and she has tried to escape 2 times.
+
+insert into animals (name , date_of_birth,weight_kg,neutered,escape_attempts)
+values('Plantmon','2022-11-15',-5.7,true,2);
+
+--Animal: His name is Squirtle. He was born on Apr 2nd, 1993, and currently weighs -12.13kg. He was not neutered and he has tried to 3 times.
+
+insert into animals (name , date_of_birth,weight_kg,neutered,escape_attempts)
+values('Squirtle','1993-04-02',-12.13,false,3);
+
+--Animal: His name is Angemon. He was born on Jun 12th, 2005, and currently weighs -45kg. He is neutered and he has tried to escape once.
+
+
+insert into animals (name , date_of_birth,weight_kg,neutered,escape_attempts)
+values('Angemon','2005-06-12',-5.7,true,2);
+
+--Animal: His name is Boarmon. He was born on Jun 7th, 2005, and currently weighs 20.4kg. He is neutered and he has tried to escape 7 times.
+
+insert into animals (name , date_of_birth,weight_kg,neutered,escape_attempts)
+values('Boarmon','2005-06-07',20.4,true,7);
+
+-- Animal: Her name is Blossom. She was born on Oct 13th, 1998, and currently weighs 17kg. She is neutered and she has tried to escape 3 times.
+
+insert into animals (name , date_of_birth,weight_kg,neutered,escape_attempts)
+values('Blossom','1998-10-13',17,true,3);
+
+-- Animal: His name is Ditto. He was born on May 14th, 2022, and currently weighs 22kg. He is neutered and he has tried to escape 4 times.
+
+insert into animals (name , date_of_birth,weight_kg,neutered,escape_attempts)
+values('Ditto','2022-05-14',22,true,4);
+
+
--- queries.sql
+++ queries.sql
@@ -42,3 +42,122 @@ select * from animals where not name='Gabumon';
  select  name from  animals where  weight_kg between 10.4 and 17.3 ;
 --   3 | Pikachu | 2021-01-07    |               1 | f        |     15.04
 --   4 | Devimon | 2017-05-12    |               5 | t        |      11.0
+------------------------------------------------------------------Day 2-------------------------------
+-- Inside a transaction update the animals table by setting the species column to unspecified. 
+--Verify that change was made. Then roll back the change and verify that species columns went back to the state before transaction.
+
+begin;
+--BEGIN
+update animals set species ='unspecified';
+--UPDATE 11
+rollback;
+--ROLLBACK
+/*
+
+
+
+
+
+
+
+
+
+
+
+*/
+begin;
+update animals set species ='digimon' where name like '%mon';
+-- UPDATE 6
+update animals set species ='pokemon' where species is null ;
+--UPDATE 5
+commit;
+-- COMMIT
+select name ,species from animals;
+--  Agumon     | digimon
+--  Gabumon    | digimon
+--  Devimon    | digimon
+--  Plantmon   | digimon
+--  Angemon    | digimon
+--  Boarmon    | digimon
+--  Pikachu    | pokemon
+--  Charmander | pokemon
+--  Squirtle   | pokemon
+--  Blossom    | pokemon
+--  Ditto      | pokemon
+
+-- Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
+begin;
+-- BEGIN
+delete from animals;
+-- DELETE 11
+rollback;
+-- ROLLBACK
+
+-- After the roll back verify if all records in the animals table still exist. After that you can start breath as usual ;)
+select * from animals ;
+--   1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon    | 2018-11-15    |               2 | t        |       8.0 | digimon
+--   4 | Devimon    | 2017-05-12    |               5 | t        |      11.0 | digimon
+--   6 | Plantmon   | 2022-11-15    |               2 | t        |      -5.7 | digimon
+--   8 | Angemon    | 2005-06-12    |               2 | t        |      -5.7 | digimon
+--   9 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
+--   3 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | pokemon
+--   5 | Charmander | 2020-02-08    |               0 | f        |       -11 | pokemon
+--   7 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | pokemon
+--  10 | Blossom    | 1998-10-13    |               3 | t        |        17 | pokemon
+--  11 | Ditto      | 2022-05-14    |               4 | t        |        22 | pokemon
+
+--Inside a transaction:
+begin;
+--1.Delete all animals born after Jan 1st, 2022.
+delete from animals where date_of_birth > '2022-1-1';
+-- DELETE 2
+--2.Create a savepoint for the transaction.
+savepoint delete_date;
+update animals set weight_kg=weight_kg *-1;
+--UPDATE 9
+--4.Rollback to the savepoint
+ROLLBACK TO delete_data;
+--5.Update all animals' weights that are negative to be their weight multiplied by -1.
+update animals set weight_kg=weight_kg*-1 where weight_kg<0;
+-- UPDATE 3
+commit;
+-- COMMIT
+select * from animals;
+--   1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon    | 2018-11-15    |               2 | t        |       8.0 | digimon
+--   4 | Devimon    | 2017-05-12    |               5 | t        |      11.0 | digimon
+--   9 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
+--   3 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | pokemon
+--  10 | Blossom    | 1998-10-13    |               3 | t        |        17 | pokemon
+--   8 | Angemon    | 2005-06-12    |               2 | t        |       5.7 | digimon
+--   5 | Charmander | 2020-02-08    |               0 | f        |        11 | pokemon
+--   7 | Squirtle   | 1993-04-02    |               3 | f        |     12.13 | pokemon
+
+---Write queries to answer the following questions:
+
+--How many animals are there?
+select count(*) from animals;
+-- 9
+
+-- How many animals have never tried to escape?
+select count(escape_attempts) from animals where escape_attempts=0;
+-- 2
+
+-- What is the average weight of animals?
+select avg(weight_kg) from animals;
+--  12.2777777777777778
+
+-- Who escapes the most, neutered or not neutered animals?
+select neutered ,max(escape_attempts) from animals group by neutered;
+--  f        |   3
+--  t        |   7
+
+-- What is the minimum and maximum weight of each type of animal?
+select species ,min(weight_kg),max(weight_kg) from animals group by species;
+--  pokemon |  11 |   17
+--  digimon | 5.7 | 20.4
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+select species ,round(avg(escape_attempts)) from animals where date_of_birth between '1990-01-01' and '2000-12-31' group by species;
+--  pokemon |     3
